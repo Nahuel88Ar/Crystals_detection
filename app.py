6af44c9271e2d781d62c304d27568386e4a1c042
Nahuel88Ar <87063711+Nahuel88Ar@users.noreply.github.com>
2025-04-13 16:18:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -494,7 +494,7 @@ if st.button("Start script 2") and bf_files and pl_files:
                 region_area_df.to_excel(writer, sheet_name='Cells', index=False)  # Shortened sheet name
                 
                 # Save the DataFrame (df_mapping) to the second sheet
-                df_mapping_with_total.to_excel(writer, sheet_name='Crystals', index=False)  # Shortened sheet name
+                df_mapping.to_excel(writer, sheet_name='Crystals', index=False)  # Shortened sheet name
                 
                 # Save the DataFrame (merged_df) to the thitd sheet
                 merged_df.to_excel(writer, sheet_name='Cells + crystals', index=False)  # Shortened sheet name
